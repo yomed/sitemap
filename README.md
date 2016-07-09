ac9f2375770238812b7f7955e427eab32871e392
Yoni Medoff <yonimedoff@gmail.com>
2016-07-09 13:40:30 -0700

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# sitemap
+
+Note: intended to run once per server load, so it's sync
+
+```sh
+npm i github:yomed/sitemap
+```
+
+## Usage
+
+```js
+var sitemap = require('sitemap');
+
+sitemap.create([
+  {
+    loc: 'http://example.com/'
+  },
+  {
+    loc: 'http://example.com/',
+    priority: '1.0'
+  }
+], 'absolute/path/sitemap.xml'); // defaults to dist/root/sitemap.xml
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var fs = require('fs');
+
+function setupEntry(entry) {
+    var xml = '';
+    ['loc', 'lastmod', 'changefreq', 'priority'].forEach(function (tag) {
+        if (entry[tag]) {
+            xml += '<' + tag + '>' + entry[tag] + '</' + tag + '>';
+        }
+    });
+    return xml && '<url>' + xml + '</url>' || '';
+}
+
+function create(entries, absolutePath) {
+    absolutePath = absolutePath || 'dist/root/sitemap.xml';
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    xml += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n';
+    xml += entries.map(function (entry) {
+        return setupEntry(entry);
+    }).join('\n');
+    xml += '\n</urlset>';
+    fs.writeFileSync(absolutePath, xml);
+}
+
+module.exports = {
+    create: create
+};
