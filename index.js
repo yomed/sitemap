0d651978ffe48a8aec777195b2c23bf8ac0b3145
Yoni Medoff <yonimedoff@gmail.com>
2020-05-03 10:35:12 -0700

    setup sync and async methods

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-'use strict';
-
-var fs = require('fs');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
 function setupEntry(entry) {
-    var xml = '';
-    ['loc', 'lastmod', 'changefreq', 'priority'].forEach(function (tag) {
-        if (entry[tag]) {
-            xml += '<' + tag + '>' + entry[tag] + '</' + tag + '>';
-        }
-    });
-    (entry.hreflangs || []).forEach(function (hreflang) {
-        if (hreflang) {
-            var lang = hreflang.lang;
-            var href = hreflang.href;
-            if (lang && href) {
-                xml += '\n<xhtml:link rel="alternate" hreflang="' + lang + '" href="' + href + '" />';
-            }
-        }
-    });
-    return xml && '<url>' + xml + '</url>' || '';
+  let xml = '';
+
+  ['loc', 'lastmod', 'changefreq', 'priority'].forEach(function (tag) {
+    if (entry[tag]) {
+      xml += '<' + tag + '>' + entry[tag] + '</' + tag + '>';
+    }
+  });
+
+  (entry.hreflangs || []).forEach((hreflang) => {
+    if (hreflang) {
+      const { lang, href } = hreflang;
+      if (lang && href) {
+        xml += '\n<xhtml:link rel="alternate" hreflang="' + lang + '" href="' + href + '" />';
+      }
+    }
+  });
+
+  return xml && '<url>' + xml + '</url>' || '';
 }
 
-function create(entries, absolutePath) {
-    absolutePath = absolutePath || 'dist/root/sitemap.xml';
-    var xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    xml += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n';
-    xml += entries.map(function (entry) {
-        return setupEntry(entry);
-    }).join('\n');
-    xml += '\n</urlset>';
-    fs.writeFileSync(absolutePath, xml);
+async function create(entries, absolutePath = 'dist/root/sitemap.xml') {
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+  xml += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n';
+  xml += entries.map((entry) => setupEntry(entry)).join('\n');
+  xml += '\n</urlset>';
+
+  await fsPromises.writeFile(absolutePath, xml);
 }
 
-module.exports = {
-    create: create
-};
+function createSync(entries, absolutePath = 'dist/root/sitemap.xml') {
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+  xml += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n';
+  xml += entries.map((entry) => setupEntry(entry)).join('\n');
+  xml += '\n</urlset>';
+
+  fs.writeFileSync(absolutePath, xml);
+}
+
+module.exports = { create, createSync };
